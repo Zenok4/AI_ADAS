4862e0a84b32421fa4aeb4413a44e6453fc765ee
tuilalinh <lepnhatlinh1@dtu.edu.vn>
2025-11-02 14:45:57 +0700

    ai of api sign

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from app.routes.predict import predict_bp
+from app.routes.sign_router import sign_bp
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(predict_bp, url_prefix="/api")
+    app.register_blueprint(sign_bp, url_prefix="/sign")
     return app
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app.config.settings import settings
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host=settings.HOST, port=settings.PORT, debug=True)
+    app.run(host=settings.HOST, port=settings.PORT, debug=False)
