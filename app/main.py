dd93fba6e7601f8e1a871f2e84f1ae93d9311f24
TrHuy04 <Letruonghuy040304@gmail.com>
2025-11-04 14:06:44 +0700

    merge ai

--- app/main.py
+++ app/main.py
@@ -1,19 +1,13 @@
 from flask import Flask
-<<<<<<< HEAD
 
 # from app.routes.predict import predict_bp
 from app.routes.drowsy_router import drowsy_bp
+from app.routes.sign_router import sign_bp
 
 
 def create_app():
     app = Flask(__name__)
     # app.register_blueprint(predict_bp, url_prefix="/api")
     app.register_blueprint(drowsy_bp, url_prefix="/drowsy")
-=======
-from app.routes.sign_router import sign_bp
-
-def create_app():
-    app = Flask(__name__)
     app.register_blueprint(sign_bp, url_prefix="/sign")
->>>>>>> origin/nlinh
     return app
